55d17f649623bfe8565715b1dc0a2a24ca0d2819
gaojiaxi <gaojiaxi2015@gmail.com>
2020-04-26 11:03:55 +0800

    tested GCS functionality

--- service/main.go
+++ service/main.go
@@ -86,13 +86,15 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
 	r.ParseMultipartForm(32 << 20)
 
-	//Parse form data
-	fmt.Println("Received one post request %s\n", r.FormValue("message"))
+	// Parse from form data.
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
 	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
-
 	p := &Post{
 		User:    "1111",
 		Message: r.FormValue("message"),
@@ -106,30 +108,35 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 
 	file, _, err := r.FormFile("image")
 	if err != nil {
-		http.Error(w, "GCS is not set up", http.StatusInternalServerError)
-		fmt.Printf("GCS is not setup %v\n", err)
-		panic(err)
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
 	}
 	defer file.Close()
 
 	ctx := context.Background()
 
+	// replace it with your real bucket name.
 	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
-
 	if err != nil {
-		http.Error(w, "GCS is not set up", http.StatusInternalServerError)
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
 		fmt.Printf("GCS is not setup %v\n", err)
-		panic(err)
+		return
 	}
 
+	// Update the media link after saving to GCS.
 	p.Url = attrs.MediaLink
+
 	// Save to ES.
 	saveToES(p, id)
 
+	// Save to BigTable.
+	//saveToBigTable(p, id)
+
 }
 
-func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (
-	*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+// Save an image to GCS.
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
 	client, err := storage.NewClient(ctx)
 	if err != nil {
 		return nil, nil, err
@@ -158,7 +165,6 @@ func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (
 	attrs, err := obj.Attrs(ctx)
 	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
 	return obj, attrs, err
-
 }
 
 // Save a post to ElasticSearch
